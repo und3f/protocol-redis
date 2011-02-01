9ad9509ac09e79d416e2ef675259745de8c3d0ba
Sergey Zasenko <d3fin3@gmail.com>
2011-02-01 20:35:55 +0200

    init

---
+++ lib/Protocol/Redis.pm
@@ -0,0 +1,255 @@
+package Protocol::Redis;
+
+use strict;
+use warnings;
+
+use List::Util;
+
+use Carp;
+
+our $VERSION = 0.1;
+
+sub new {
+    my $class = shift;
+    $class = ref $class if ref $class;
+
+    my $self = {@_};
+    bless $self, $class;
+
+    $self->newline($self->{newline});
+    $self->{_commands} = [];
+    $self->_change_state(\&_state_new_command);
+
+    $self;
+}
+
+sub newline {
+    my ($self, $newline) = @_;
+
+    $self->{_newline} = $newline or "\r\n";
+}
+
+sub get_command {
+    my ($self) = @_;
+    shift @{$self->{_commands}};
+}
+
+sub on_command {
+    my ($self, $cb) = @_;
+    $self->{_on_command_cb} = $cb;
+}
+
+sub parse {
+    my ($self, $chunk) = @_;
+
+    # Just pass chunk to current vertex
+    $self->{_state}->($self, $chunk);
+}
+
+sub _change_state {
+    my ($self, $new_state, $chunk) = @_;
+
+    $self->{_state} = $new_state;
+
+    # Pass rest of chunk to new vertex
+    $new_state->($self, $chunk) if $chunk;
+}
+
+sub _command_parsed {
+    my ($self, $chunk) = @_;
+
+    my $command = delete $self->{_cmd};
+
+    if (my $cb = $self->{_on_command_cb}) {
+        $cb->($self, $command);
+    }
+    else {
+        push @{$self->{_commands}}, $command;
+    }
+    $self->_change_state(\&_state_new_command, $chunk);
+}
+
+sub _state_new_command {
+    my ($self, $chunk) = @_;
+
+    $self->{_cmd} = {type => undef, data => undef};
+
+    my $cmd = substr $chunk, 0, 1, '';
+
+    if (List::Util::first { $cmd eq $_ } (qw/+ - :/)) {
+        $self->{_cmd}{type} = $cmd;
+        $self->{_state_cb} = \&_command_parsed;
+        $self->_change_state(\&_state_string_command, $chunk);
+    }
+    elsif ($cmd eq '$') {
+        $self->{_cmd}{type} = $cmd;
+        $self->{_state_cb} = \&_command_parsed;
+        $self->_change_state(\&_state_bulk_command, $cmd . $chunk);
+    }
+    elsif ($cmd eq '*') {
+        $self->{_cmd}{type} = $cmd;
+        $self->{_state_cb} = \&_command_parsed;
+        $self->_change_state(\&_state_multibulk_command, $chunk);
+
+    }
+    else {
+        Carp::croak qq/Unexcpected input "$cmd"/;
+    }
+}
+
+sub _state_string_command {
+    my ($self, $chunk) = @_;
+
+    my $str = $self->{_state_string} .= $chunk;
+    my $i = index $str, $self->newline;
+
+    if ($i >= 0) {
+        my $result = substr $str, 0, $i, '';
+
+        # Delete newline
+        substr $str, 0, length($self->newline), '';
+
+        delete $self->{_state_string};
+
+        $self->{_cmd}{data} = $result;
+        $self->{_state_cb}->($self, $str);
+    }
+}
+
+sub _state_bulk_command {
+    my ($self, $chunk) = @_;
+
+    my $bulk_state_cb = $self->{_state_cb};
+
+    # Read bulk command size
+    $self->{_state_cb} = sub {
+        my ($self, $chunk) = @_;
+
+        $self->{_bulk_size} = delete $self->{_cmd}{data};
+
+        # Delete starting '$'
+        substr $self->{_bulk_size}, 0, 1, "";
+
+        $self->{_state_cb} = $bulk_state_cb;
+        $self->_change_state(\&_state_bulk_command_data, $chunk);
+
+    };
+    $self->_change_state(\&_state_string_command, $chunk);
+}
+
+sub _state_bulk_command_data {
+    my ($self, $chunk) = @_;
+
+    my $str = $self->{_state_string} .= $chunk;
+    if (length $str >= $self->{_bulk_size} + length $self->newline) {
+        my $result = substr $str, 0, $self->{_bulk_size}, '';
+
+        # Delete ending newline
+        substr $str, 0, length $self->newline, '';
+
+        delete $self->{_state_string};
+        delete $self->{_bulk_size};
+
+        $self->{_cmd}{data} = $result;
+        $self->{_state_cb}->($self, $str);
+    }
+}
+
+sub _state_multibulk_command {
+    my ($self, $chunk) = @_;
+
+    my $mbulk_state_cb = delete $self->{_state_cb};
+    my $data           = [];
+    my $mbulk_process;
+
+    $mbulk_process = sub {
+        my ($self, $chunk) = @_;
+
+        push @$data, delete $self->{_cmd}{data};
+
+        if (scalar @$data == $self->{_mbulk_arg_num}) {
+
+            # Cleanup
+            $mbulk_process = undef;
+            delete $self->{_mbulk_arg_num};
+            $self->{_cmd}{data} = $data;
+            delete $self->{_state_cb};
+
+            $mbulk_state_cb->($self, $chunk);
+        }
+        else {
+
+            # read next string
+            $self->{_state_cb} = $mbulk_process;
+            $self->_change_state(\&_state_bulk_command, $chunk);
+        }
+    };
+
+    $self->{_state_cb} = sub {
+        my ($self, $chunk) = @_;
+
+        # Number of Multi-Bulk command
+        my $num = $self->{_mbulk_arg_num} = delete $self->{_cmd}{data};
+        if ($num < 1) {
+            $mbulk_process = undef;
+            $self->{_cmd}{data} = [];
+            $mbulk_state_cb->($self, $chunk);
+        }
+        else {
+
+            # We got commands
+            $self->{_state_cb} = $mbulk_process;
+            $self->_change_state(\&_state_bulk_command, $chunk);
+        }
+    };
+
+    # Get number of commands
+    $self->_change_state(\&_state_string_command, $chunk);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Protocol::Redis - Redis protocol parser
+
+=head1 DESCRIPTION
+
+Redis protocol parser.
+
+=head1 METHODS
+
+=head2 C<parse>
+
+    $redis->parse("*2\r\n$4ping\r\n\r\n");
+
+Parse Redis protocol chunk.
+
+=head2 C<get_command>
+
+    while (my $command = $redis->get_command) {
+        ...
+    }
+
+Get parsed command or undef.
+
+=head2 C<on_command>
+
+    $redis->on_command(sub {
+        my ($redis, $command) = @_;
+
+    }
+
+Calls callback on each parsed command.
+
+=head1 AUTHOR
+
+Sergey Zasenko, C<undef@cpan.org>.
+
+=head1 COPYRIGHT (C) 2011, Sergey Zasenko.
+
+This program is free software, you can redistribute it and/or modify it under
+the same terms as Perl 5.10.
+
+=cut
---
+++ t/redis.t
@@ -0,0 +1,100 @@
+#!usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 14;
+
+use_ok 'Protocol::Redis';
+
+my $redis = new_ok 'Protocol::Redis';
+
+# Simple test
+$redis->parse("+test\r\n");
+
+is_deeply $redis->get_command,
+  {type => '+', data => 'test'},
+  'simple command';
+
+is_deeply $redis->get_command,
+  undef,
+  'queue is empty';
+
+$redis->parse(":test2\r\n");
+
+is_deeply $redis->get_command,
+  {type => ':', data => 'test2'},
+  'simple number';
+
+# Chunked command
+$redis->parse('-tes');
+$redis->parse("t2\r\n");
+is_deeply $redis->get_command,
+  {type => '-', data => 'test2'},
+  'chunked string';
+
+# Two commands together
+$redis->parse("+test");
+$redis->parse("1\r\n-test");
+$redis->parse("2\r\n");
+is_deeply
+  [$redis->get_command, $redis->get_command],
+  [{type => '+', data => 'test1'}, {type => '-', data => 'test2'}],
+  'first stick command';
+
+# Bulk command
+$redis->parse("\$4\r\ntest\r\n");
+is_deeply $redis->get_command,
+  {type => '$', data => 'test'},
+  'simple bulk command';
+
+$redis->parse("\$5\r\ntes");
+$redis->parse("t2\r\n");
+is_deeply $redis->get_command,
+  {type => '$', data => 'test2'},
+  'splitted bulk command';
+
+# Multi bulk command!
+$redis->parse("*1\r\n\$4\r\ntest\r\n");
+
+is_deeply $redis->get_command,
+  {type => '*', data => ['test']},
+  'simple multibulk command';
+
+# Multi bulk command with multiple arguments
+$redis->parse("*3\r\n\$5\r\ntest1\r\n");
+$redis->parse("\$5\r\ntest2\r\n");
+$redis->parse("\$5\r\ntest3\r\n");
+
+is_deeply $redis->get_command,
+  {type => '*', data => [qw/test1 test2 test3/]},
+  'multi argument multi-bulk command';
+
+$redis->parse("*0\r\n");
+is_deeply $redis->get_command,
+  {type => '*', data => []},
+  'multi-bulk nil result';
+
+# Does it work?
+$redis->parse("\$4\r\ntest\r\n");
+is_deeply $redis->get_command,
+  {type => '$', data => 'test'},
+  'everything still works';
+
+# Parsing with cb
+my $r = [];
+$redis->on_command(
+    sub {
+        my ($redis, $command) = @_;
+
+        push @$r, $command;
+    }
+);
+
+$redis->parse("+foo\r\n");
+$redis->parse("\$3\r\nbar\r\n");
+
+is_deeply $r,
+  [{type => '+', data => 'foo'}, {type => '$', data => 'bar'}],
+  'parsing with callback';
+
