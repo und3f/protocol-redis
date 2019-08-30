cfc164478056a67acfe6e0e3bd72b7c1e21b67e6
Sergey Zasenko <sergii@zasenko.name>
2019-08-31 00:14:13 +0300

    added Travis

--- benchmark.pl
+++ benchmark.pl
@@ -8,22 +8,80 @@ use Benchmark qw(:all :hireswallclock);
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use Protocol::Redis;
+use Protocol::Redis::Faster;
+use Protocol::Redis::Test;
 
 my $redis = Protocol::Redis->new(api => 1);
+my $redis_fast = Protocol::Redis::Faster->new(api => 1);
 
 $redis->on_message(sub { });
 
+sub encode {
+    my $redis = shift;
+
+    # Encode message
+    $redis->encode({type => '+', data => 'OK'});
+    $redis->encode({type => '-', data => 'ERROR'});
+    $redis->encode({type => ':', data => '5'});
+
+    # Encode bulk message
+    $redis->encode({type => '$', data => 'test'});
+    $redis->encode({type => '$', data => "\0\r\n"});
+    $redis->encode({type => '$', data => undef});
+
+    # Encode multi-bulk
+    $redis->encode({type => '*', data => [{type => '$', data => 'test'}]});
+
+    $redis->encode(
+        {   type => '*',
+            data => [
+                {type => '$', data => 'test1'}, {type => '$', data => 'test2'}
+            ]
+        }
+      );
+
+    $redis->encode({type => '*', data => []});
+
+    $redis->encode({type => '*', data => undef});
+
+    $redis->encode(
+        {   type => '*',
+            data => [
+                {type => '$', data => 'foo'},
+                {type => '$', data => undef},
+                {type => '$', data => 'bar'}
+            ]
+        }
+      );
+}
+
+cmpthese(2**17, {
+        'P:R:F encode' => sub { encode($redis_fast) },
+        'P:R encode' => sub { encode($redis) },
+    });
+
 my $status_message = $redis->encode({type => '+', data => 'OK'});
-timethese(
-    0,
-    {   '1. Status parse' => sub { $redis->parse($status_message) },
-        '2. Splitted status parse' =>
+cmpthese(
+    -1,
+    {   'P:R Status parse' => sub { $redis->parse($status_message) },
+        'P:R:F Status parse' => sub { $redis_fast->parse($status_message) },
+});
+
+my $a = 'cmpthese',(
+    -1,
+    { 
+        'P:R Splitted status parse' =>
           sub { $redis->parse("+OK"); $redis->parse("\r\n") },
+        'P:R:F Splitted status parse' =>
+          sub { $redis_fast->parse("+OK"); $redis->parse("\r\n") },
     }
 );
 
-my $bulk_message = $redis->encode({type => '$', data => 'test'});
-timethese(0, {'1. Bulk parse' => sub { $redis->parse($bulk_message) },});
+my $bulk_message = $redis->encode({type => '$', data => 'test'}) . $redis->encode({type => '$', data => undef});
+cmpthese(-1, {
+        'Bulk parse P:R' => sub { $redis->parse($bulk_message) },
+        'Bulk parse P:R:F' => sub { $redis_fast->parse($bulk_message) },
+});
 
 my $mbulk_message = $redis->encode(
     {   type => '*',
@@ -31,4 +89,7 @@ my $mbulk_message = $redis->encode(
           [{type => '$', data => 'test1'}, {type => '$', data => 'test2'}]
     }
 );
-timethese(0, {'Multi-Bulk parse' => sub { $redis->parse($mbulk_message) },});
+cmpthese(-1, {
+        'P:R Multi-Bulk parse' => sub { $redis->parse($mbulk_message) },
+        'P:R:F Multi-Bulk parse' => sub { $redis_fast->parse($mbulk_message) },
+    });
